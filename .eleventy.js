67cf80f7fa7a9df07918757acd3e0631531eeef4
guybriggs <guybriggs8278@gmail.com>
2026-06-09 21:39:55 +1000

    Add shine images with parallax

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src");
   eleventyConfig.addPassthroughCopy("CNAME");
 
   const { DateTime } = require("luxon");
+
+  const SHINE_ORDER = [
+    { name: "YELLOW", angle: 0 },
+    { name: "LIME", angle: 30 },
+    { name: "GREEN", angle: 60 },
+    { name: "TURQ", angle: 90 },
+    { name: "BLUE", angle: 120 },
+    { name: "BLURPLE", angle: 150 },
+    { name: "PURPLE", angle: 180 },
+    { name: "PINK", angle: 210 },
+    { name: "RED", angle: 240 },
+    { name: "ORANGE", angle: 270 },
+    { name: "GOLD", angle: 300 },
+    { name: "MANGO", angle: 330 },
+  ];
+
+  eleventyConfig.addGlobalData("shineImages", () => {
+    const shineDir = path.join(__dirname, "src/img/2026/shine");
+    const files = fs
+      .readdirSync(shineDir)
+      .filter((file) => /\.(png|jpe?g|gif|webp)$/i.test(file));
+
+    return SHINE_ORDER.flatMap(({ name, angle }) => {
+      const file = files.find(
+        (f) => path.parse(f).name.toUpperCase() === name
+      );
+      if (!file) {
+        console.warn(`Shine image not found: ${name}`);
+        return [];
+      }
+      return [{ src: `/src/img/2026/shine/${file}`, angle }];
+    });
+  });
 
   eleventyConfig.addFilter("sydneyTime", (dateInput, format = "h:mma") => {
     const dt = DateTime.fromISO(dateInput, { zone: 'Australia/Sydney' });
